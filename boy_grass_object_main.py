685d578a51e11769a2492b9c41c6501a649a0d35
sunwooseungyeon <swsy09@naver.com>
2016-10-31 23:39:37 +0900

    Add files via upload

---
+++ boy_grass_object_main.py
@@ -0,0 +1,84 @@
+from pico2d import *
+
+
+class Back:
+    def __init__(self):
+        self.image = load_image('back.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+
+
+class Back1:
+    def __init__(self):
+        self.image = load_image('back1.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+
+
+class Mons1:
+    def __init__(self):
+        self.image = load_image('mons1.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('pac.png')
+        self.dir = 1
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += self.dir
+        if self.x >= 800:
+            self.dir = -1
+        elif self.x <= 0:
+            self.dir = 1
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.type == SDLK_ESCAPE:
+            running = False
+
+
+# fill here for boy, grass
+running = True
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    pass
+
+
+def main():
+    pass
+
+
+# fill here
+
+
---
+++ main_state.py
@@ -0,0 +1,115 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import title_state
+
+
+
+name = "MainState"
+
+pac = None
+back = None
+back1 = None
+mons1 = None
+font = None
+
+
+
+class Back:
+    def __init__(self):
+        self.image = load_image('back.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+class Back1:
+    def __init__(self):
+        self.image = load_image('back1.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+class Mons1:
+    def __init__(self):
+        self.image = load_image('mons1.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+
+class Pac:
+    def __init__(self):
+        self.x, self.y = 300, 318
+        self.frame = 0
+        self.image = load_image('Pac-Man.png')
+        self.dir = 1
+
+    def update(self):
+        self.x += self.dir
+        if self.x >= 800:
+            self.dir = -1
+        elif self.x <= 0:
+            self.dir = 1
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+def enter():
+    global back,back1,mons1,pac
+    back = Back()
+    back1 = Back1()
+    mons1 = Mons1()
+    pac = Pac()
+    pass
+
+
+def exit():
+    global back, back1, mons1, pac
+    del(back)
+    del(back1)
+    del(mons1)
+    del(pac)
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+    pass
+
+
+def update():
+    pac.update()
+    pass
+
+
+def draw():
+    clear_canvas()
+    back.draw()
+    back1.draw()
+    mons1.draw()
+    pac.draw()
+    update_canvas()
+    pass
+
+
+
+
+
