9a54235d43af4d2c758ec639070b93b38ec37467
sunwooseungyeon <swsy09@naver.com>
2016-11-14 23:58:05 +0900

    Add files via upload

--- don1.py
+++ don1.py
@@ -7,5 +7,10 @@ class Don1:
         self.image = load_image('don1.png')
 
     def draw(self):
+        self.image.draw(60, 88)
+        self.image.draw(95, 88)
+        self.image.draw(130, 88)
+        self.image.draw(165, 88)
+        self.image.draw(200, 88)
         self.image.draw(400, 310)
         self.image.draw(430, 310)
